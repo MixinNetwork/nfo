4acc2e39a7e521932ec8d67d54d5591d81c83b44
Cedric Fung <cedric@vec.io>
2021-10-17 05:49:06 +0800

    fix transaction signing

--- mtg/group.go
+++ mtg/group.go
@@ -7,10 +7,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/MixinNetwork/mixin/common"
-	"github.com/MixinNetwork/mixin/crypto"
 	"github.com/fox-one/mixin-sdk-go"
-	"github.com/shopspring/decimal"
 )
 
 type Group struct {
@@ -99,81 +96,6 @@ func (grp *Group) BuildTransaction(ctx context.Context, assetId string, receiver
 		UpdatedAt: time.Now(),
 	}
 	return grp.store.WriteTransaction(traceId, tx)
-}
-
-func (grp *Group) signTransaction(ctx context.Context, tx *Transaction) ([]byte, error) {
-	outputs, err := grp.store.ListOutputsForTransaction(tx.TraceId)
-	if err != nil {
-		return nil, err
-	}
-	if len(outputs) == 0 {
-		outputs, err = grp.store.ListOutputsForAsset(mixin.UTXOStateUnspent, tx.AssetId, 36)
-		if err != nil {
-			return nil, err
-		}
-	}
-	ver := common.NewTransaction(crypto.NewHash([]byte(tx.AssetId)))
-	ver.Extra = encodeMixinExtra(tx.TraceId, tx.Memo)
-
-	var total common.Integer
-	for _, out := range outputs {
-		total = total.Add(common.NewIntegerFromString(out.Amount.String()))
-		ver.AddInput(crypto.Hash(out.TransactionHash), out.OutputIndex)
-	}
-	if total.Cmp(common.NewIntegerFromString(tx.Amount)) < 0 {
-		return nil, fmt.Errorf("insufficient %s %s", total, tx.Amount)
-	}
-	inputs := []*mixin.GhostInput{{
-		Receivers: tx.Receivers,
-		Index:     0,
-		Hint:      tx.TraceId,
-	}, {
-		Receivers: grp.members,
-		Index:     1,
-		Hint:      tx.TraceId,
-	}}
-	keys, err := grp.mixin.BatchReadGhostKeys(ctx, inputs)
-	if err != nil {
-		return nil, err
-	}
-
-	amount, err := decimal.NewFromString(tx.Amount)
-	if err != nil {
-		return nil, err
-	}
-	out := keys[0].DumpOutput(uint8(tx.Threshold), amount)
-	ver.Outputs = append(ver.Outputs, newCommonOutput(out))
-
-	if diff := total.Sub(common.NewIntegerFromString(tx.Amount)); diff.Sign() > 0 {
-		amount, err := decimal.NewFromString(diff.String())
-		if err != nil {
-			return nil, err
-		}
-		out := keys[0].DumpOutput(uint8(grp.threshold), amount)
-		ver.Outputs = append(ver.Outputs, newCommonOutput(out))
-	}
-
-	raw := hex.EncodeToString(ver.AsLatestVersion().Marshal())
-	req, err := grp.mixin.CreateMultisig(ctx, mixin.MultisigActionSign, raw)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, out := range outputs {
-		out.State = OutputStateSigned
-		out.SignedBy = ver.AsLatestVersion().PayloadHash().String()
-		out.SignedTx = raw
-	}
-	err = grp.store.WriteOutputs(outputs, tx.TraceId)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err = grp.mixin.SignMultisig(ctx, req.RequestID, grp.pin)
-	if err != nil {
-		return nil, err
-	}
-	return ver.AsLatestVersion().Marshal(), nil
 }
 
 func (grp *Group) handleUnspentOutputs(ctx context.Context) error {
--- mtg/transaction.go
+++ mtg/transaction.go
@@ -1,13 +1,16 @@
 package mtg
 
 import (
+	"context"
 	"encoding/hex"
+	"fmt"
 	"time"
 
 	"github.com/MixinNetwork/mixin/common"
 	"github.com/MixinNetwork/mixin/crypto"
 	"github.com/fox-one/mixin-sdk-go"
 	"github.com/gofrs/uuid"
+	"github.com/shopspring/decimal"
 )
 
 const (
@@ -30,6 +33,86 @@ type Transaction struct {
 type MixinExtraPack struct {
 	T uuid.UUID
 	M string `msgpack:",omitempty"`
+}
+
+func (grp *Group) signTransaction(ctx context.Context, tx *Transaction) ([]byte, error) {
+	outputs, err := grp.store.ListOutputsForTransaction(tx.TraceId)
+	if err != nil {
+		return nil, err
+	}
+	if len(outputs) == 0 {
+		outputs, err = grp.store.ListOutputsForAsset(mixin.UTXOStateUnspent, tx.AssetId, 36)
+		if err != nil || len(outputs) == 0 {
+			return nil, err
+		}
+	}
+	if outputs[0].State == OutputStateSpent {
+		tx, _ := decodeTransactionWithExtra(outputs[0].SignedTx)
+		return tx.Marshal(), nil
+	}
+
+	ver := common.NewTransaction(crypto.NewHash([]byte(tx.AssetId)))
+	ver.Extra = encodeMixinExtra(tx.TraceId, tx.Memo)
+
+	var total common.Integer
+	for _, out := range outputs {
+		total = total.Add(common.NewIntegerFromString(out.Amount.String()))
+		ver.AddInput(crypto.Hash(out.TransactionHash), out.OutputIndex)
+	}
+	if total.Cmp(common.NewIntegerFromString(tx.Amount)) < 0 {
+		return nil, fmt.Errorf("insufficient %s %s", total, tx.Amount)
+	}
+
+	keys, err := grp.mixin.BatchReadGhostKeys(ctx, []*mixin.GhostInput{{
+		Receivers: tx.Receivers,
+		Index:     0,
+		Hint:      tx.TraceId,
+	}, {
+		Receivers: grp.members,
+		Index:     1,
+		Hint:      tx.TraceId,
+	}})
+	if err != nil {
+		return nil, err
+	}
+
+	amount, err := decimal.NewFromString(tx.Amount)
+	if err != nil {
+		return nil, err
+	}
+	out := keys[0].DumpOutput(uint8(tx.Threshold), amount)
+	ver.Outputs = append(ver.Outputs, newCommonOutput(out))
+
+	if diff := total.Sub(common.NewIntegerFromString(tx.Amount)); diff.Sign() > 0 {
+		amount, err := decimal.NewFromString(diff.String())
+		if err != nil {
+			return nil, err
+		}
+		out := keys[1].DumpOutput(uint8(grp.threshold), amount)
+		ver.Outputs = append(ver.Outputs, newCommonOutput(out))
+	}
+
+	raw := hex.EncodeToString(ver.AsLatestVersion().Marshal())
+	req, err := grp.mixin.CreateMultisig(ctx, mixin.MultisigActionSign, raw)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, out := range outputs {
+		out.State = OutputStateSigned
+		out.SignedBy = ver.AsLatestVersion().PayloadHash().String()
+		out.SignedTx = raw
+	}
+	err = grp.store.WriteOutputs(outputs, tx.TraceId)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err = grp.mixin.SignMultisig(ctx, req.RequestID, grp.pin)
+	if err != nil {
+		return nil, err
+	}
+	return ver.AsLatestVersion().Marshal(), nil
 }
 
 func decodeTransactionWithExtra(s string) (*common.VersionedTransaction, *MixinExtraPack) {
